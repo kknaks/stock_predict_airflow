8a20c767613a22f7291a601651e95b746f8d3ef1
kknaks <benesia93@naver.com>
2026-01-28 22:56:27 +0900

    .

--- plugins/operators/__init__.py
+++ plugins/operators/__init__.py
@@ -1,11 +1,3 @@
-from operators.stock_data_operators import (
-    SymbolLoaderOperator,
-    MarketDataOperator,
-    StockDataOperator
-)
-
-__all__ = [
-    'SymbolLoaderOperator',
-    'MarketDataOperator',
-    'StockDataOperator'
-]
+# Operators package
+# 각 operator는 직접 import하여 사용
+# 예: from operators.gap_up_filter_operator import GapUpFilterOperator
